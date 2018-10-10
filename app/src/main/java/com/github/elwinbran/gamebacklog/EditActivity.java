1ed6efc797651f2784a2805e4f8a0f3f54c3640e
ElwinBran <elwinbran@gmail.com>
2018-10-10 20:41:02 +0200

    Added second activity, where user can edit the a game backlog entry

---
+++ app/src/main/java/com/github/elwinbran/gamebacklog/EditActivity.java
@@ -0,0 +1,17 @@
+package com.github.elwinbran.gamebacklog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_edit);
+        //TODO check if edit or add
+
+        //TODO add FAB onClick that adds stuff to DB? 
+    }
+}
